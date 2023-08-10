6ca8b9b34e83299411827cef4bf86b75b62b5ac5
Gustavo Luis <gferreiraluis@outlook.com>
2023-08-10 18:35:26 -0300

    create manager from post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,15 @@
+from blog.models import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 
 def index(request):
-    paginator = Paginator(posts, 9)
+   
+    posts = Post.objects.get_published()
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -19,11 +23,7 @@ def index(request):
 
 
 def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(
+     return render(
         request,
         'blog/pages/page.html',
         {
@@ -33,10 +33,6 @@ def page(request):
 
 
 def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
     return render(
         request,
         'blog/pages/post.html',
